bf55d327481c5496adc1b1a7d5c858d52d4828f1
Camilo Lopez <camilino123@gmail.com>
2019-03-06 18:48:47 -0500

    Models updated

--- src/main/java/edu/eci/ieti/taskplanner/Model/Task.java
+++ src/main/java/edu/eci/ieti/taskplanner/Model/Task.java
@@ -1,7 +1,47 @@
 package edu.eci.ieti.taskplanner.Model;
 
+import java.util.Date;
+
 /**
  *
  */
 public class Task {
+
+    private Integer owner;
+    private String description;
+
+    public Integer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Integer owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(String responsible) {
+        this.responsible = responsible;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    private String responsible;
+    private Date dueDate;
 }
--- src/main/java/edu/eci/ieti/taskplanner/Model/User.java
+++ src/main/java/edu/eci/ieti/taskplanner/Model/User.java
@@ -4,4 +4,78 @@ package edu.eci.ieti.taskplanner.Model;
  *
  */
 public class User {
+
+    private Integer identification;
+    private String fullFirstName;
+    private String fullLastName;
+    private String email;
+    private String userName;
+    private Integer phoneNumber;
+    private String occupation;
+    private String password;
+
+    public Integer getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(Integer identification) {
+        this.identification = identification;
+    }
+
+    public String getFullFirstName() {
+        return fullFirstName;
+    }
+
+    public void setFullFirstName(String fullFirstName) {
+        this.fullFirstName = fullFirstName;
+    }
+
+    public String getFullLastName() {
+        return fullLastName;
+    }
+
+    public void setFullLastName(String fullLastName) {
+        this.fullLastName = fullLastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
